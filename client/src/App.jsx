57ddd3aa75afece2b827cc0559336b96819a7ff7
Shourya Gupta <shouryaguptag11@gmail.com>
2024-09-13 16:11:21 +0530

    Complete navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,19 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="projects" element={<Projects />} />
+        <Route path="/projects" element={<Projects />} />
         
 
       </Routes>
