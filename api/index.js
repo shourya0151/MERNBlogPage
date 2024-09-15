f7a8695954358150d1dbd86ee5ed52adf7aa8849
Shourya Gupta <shouryaguptag11@gmail.com>
2024-09-15 23:29:39 +0530

    Database Connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+(async()=> {
+    await mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log('MongoDb is conneted');
+    }).catch((err)=>{
+        console.log(err);
+    });
+})();//USING IIFE 
+
 
 const app = express();
+
+
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
