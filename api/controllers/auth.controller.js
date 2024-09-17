fd01a2a780dc3df2c29d17e2acfbfb35f69e7ed0
Shourya Gupta <shouryaguptag11@gmail.com>
2024-09-17 13:16:16 +0530

    Sign API

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../model/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 
 
@@ -17,7 +18,7 @@ export const signup = async(req,res,next)=>{
     }
 
 
-    //hahs password
+    //hashed password
     const hashedPassword = bcryptjs.hashSync(password,10);
 
     const newUser = new User({
@@ -31,6 +32,48 @@ export const signup = async(req,res,next)=>{
         res.json('Signup successful');
 
     } catch(err){
+        next(err);
+    }
+};
+
+
+export const signin = async(req,res,next)=>{
+    const {email,password} = req.body;
+
+    if(!email || !password 
+        || email === '' 
+        || password === ''
+        )
+    {
+        next(errorHandler(400,"All fields are required"));
+    }
+
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            next(errorHandler(404,"User not found"));
+        }
+        
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword){
+            return next(errorHandler(400,'Inavlid userName or password'));
+        }
+
+        const token = jwt.sign({ id: validUser.__id }, process.env.SECRET_KEY, {
+            expiresIn: '1h',
+        });
+
+        //separate the password before sending it back
+        const {password:pass , ...rest} = validUser._doc;
+        //console.log(validUser._doc);
+
+        res
+            .status(200)
+            .cookie("access_token",token,{
+                httpOnly: true,
+            })
+            .json(rest);
+    }catch(err){
         next(err);
     }
 };
