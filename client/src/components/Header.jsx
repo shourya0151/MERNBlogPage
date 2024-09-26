6e7eb2c8b59b02f1037df1537d35d4b1c949d59d
Shourya Gupta <shouryaguptag11@gmail.com>
2024-09-26 10:39:37 +0530

    Header profile pic to sign in

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,16 @@
-import { Button, Navbar, NavbarToggle } from 'flowbite-react'
+import { Avatar, Button, Dropdown, Navbar, NavbarToggle } from 'flowbite-react'
 import { Link,useLocation } from 'react-router-dom';
 import { TextInput } from 'flowbite-react';
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from 'react-icons/fa'
+import { useSelector } from 'react-redux'
+
+
 
 
 export default function Header() {
     const path = useLocation().pathname;
+    const { currentUser } = useSelector((state) => state.user);
 
   return (
     <Navbar className='border-b-2'>
@@ -32,13 +36,41 @@ export default function Header() {
             <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
                 <FaMoon/>
             </Button>
-            <Link to='/sign-in'>
+            {currentUser ? (
+                <Dropdown
+                    arrowIcon = {false}
+                    inline
+                    label={
+                        
+                        <Avatar
+                            alt = 'user'
+                            img = {currentUser.profilePicture}
+                            rounded
+                        />
+                    }
+                >
+                    <Dropdown.Header>
+                        <span className='block text-sm'>@{currentUser.username}</span>
+                        <span className='block text-sm font-medium truncate'>
+                            @{currentUser.email}</span>
+                    </Dropdown.Header>
+                    <Link to={'/dashboard?tab=profile'}>
+                        <Dropdown.Item>Profile</Dropdown.Item>
+                    </Link>
+                    <Dropdown.Divider></Dropdown.Divider>
+                    <Dropdown.Item>SignOut</Dropdown.Item>
+
+                </Dropdown>
+            ):
+                <Link to='/sign-in'>
                 <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-400 via-purple-500 to-pink-400 group-hover:from-purple-400 group-hover:via-purple-500 group-hover:to-pink-400 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
                     <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                         Sign In
                     </span>
                 </button>
-            </Link>
+                </Link>
+            }
+            
             <NavbarToggle/>
         </div>
         <Navbar.Collapse>
