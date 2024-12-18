ae46ce5220fb176fa32389982d2b9fd4191dd6b1
Shourya Gupta <shouryaguptag11@gmail.com>
2024-12-18 10:02:41 +0530

    dashside bar

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,9 +1,11 @@
 import { Sidebar } from "flowbite-react";
-import {HiUser, HiArrowSmRight} from 'react-icons/hi'
+import {HiUser, HiArrowSmRight, HiDocumentText} from 'react-icons/hi'
 import { Link, useLocation } from 'react-router-dom'
 import {useEffect, useState } from 'react'
 import { signoutUserSuccess } from "../redux/user/userSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { current } from "@reduxjs/toolkit";
+
 
 export default function DashSidebar() {
 
@@ -11,6 +13,7 @@ export default function DashSidebar() {
     const location = useLocation();
     const [tab, setTab] = useState('');
     const dispatch = useDispatch()
+    const { currentUser } = useSelector((state) => state.user);
   
     useEffect(()=>{
       const urlParams = new URLSearchParams(location.search)
@@ -43,14 +46,28 @@ export default function DashSidebar() {
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
-        <Sidebar.ItemGroup>
-          
-            <Sidebar.Item href="/dashboard?tab=profile"active={tab === 'profile'} 
+        <Sidebar.ItemGroup className="flex flex-col gap-1">
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item active={tab === 'profile'} 
             icon={HiUser} 
-            label={'User'} 
-            labelColor='dark'>
+            label={ currentUser.isAdmin ? 'Admin' : 'User'} 
+            labelColor='dark'
+            as = 'div'>
             Profile
-          </Sidebar.Item>
+            </Sidebar.Item>
+          </Link>
+          {currentUser.isAdmin && (
+            
+          
+            <Link to='/dashboard?tab=posts'>
+              <Sidebar.Item
+                active = {tab === 'posts'}
+                icon = {HiDocumentText}
+                as = 'div'>
+                  Posts
+              </Sidebar.Item>
+            </Link>
+          )}
           
            
           
